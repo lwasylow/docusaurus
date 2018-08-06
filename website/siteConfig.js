4ab88a32fcbf526c5d4a247343f1abf2efa8def2
lwasylow <lwasylow@gmail.com>
2018-08-06 22:35:49 +0100

    update baseurl

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -24,7 +24,7 @@ const siteConfig = {
   title: 'utPLSQL Documentation' /* title for your website */,
   tagline: 'Documentation',
   url: 'https://utplsql.org' /* your website url */,
-  baseUrl: '/pages/{organizationName}/{projectName}/' /* base url for your project */,
+  baseUrl: '/pages/lwasylow/docusaurus/' /* base url for your project */,
   // Used for publishing and more
   projectName: 'docusaurus',
   organizationName: 'lwasylow',
